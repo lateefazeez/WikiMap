4aa9f429fb603d4555aafd54d9a71d5172ab96f9
Freem11 <freem1985@gmail.com>
2021-10-15 19:35:38 -0700

    pins and layers table schemas

--- db/schema/05_widgets.sql
+++ db/schema/05_widgets.sql
@@ -1,8 +1,21 @@
 -- Drop and recreate Widgets table (Example)
 
-DROP TABLE IF EXISTS widgets CASCADE;
-CREATE TABLE widgets (
+DROP TABLE IF EXISTS pins CASCADE;
+CREATE TABLE pins (
   id SERIAL PRIMARY KEY NOT NULL,
-  user_id INTEGER REFERENCES users(id),
-  name VARCHAR(255) NOT NULL
+  contributor_id INTEGER NOT NULL REFERENCES maps(id) ON DELETE CASCADE,
+  map_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255) NOT NULL,
+  description TEXT,
+  image VARCHAR(255) NOT NULL,
+  longitude REAL,
+  latitude REAL,
+  layer_id INTEGER NOT NULL REFERENCES layers(id) ON DELETE CASCADE
+);
+
+DROP TABLE IF EXISTS layers CASCADE;
+CREATE TABLE layers (
+  id SERIAL PRIMARY KEY NOT NULL,
+  title VARCHAR(255) NOT NULL,
+  user INTEGER NOT NULL REFERENCES maps(id) ON DELETE CASCADE
 );
