2fc0b5a5b00917bfc6b20822f0d8aaf2ad66d9c9
Freem11 <freem1985@gmail.com>
2021-10-19 11:29:31 -0700

    single map entry fixed

--- routes/getMap.js
+++ routes/getMap.js
@@ -7,50 +7,22 @@ const router = express.Router();
 const db = require("../lib/mapqueries.js");
 const dc = require("../lib/pinQueries.js");
 
-// router.get("/:id", (req, res) => {
-//   const username = req.session.username;
-//   const map_id = req.params.id;
-//   db.getSingleMap(map_id)
-//     .then(currentMap => {
-
-//       res.render("map", { mapName: currentMap[0].name, user: username });
-//     })
-//     .catch(err => {
-//       res
-//         .status(500)
-//         .json({ error: err.message });
-//     });
-// });
-
 router.get("/:id", (req, res) => {
+  const username = req.session.username;
+  const map_id = req.params.id;
+  db.getSingleMap(map_id)
+    .then(currentMap => {
 
+      dc.getPinsByMap(map_id)
+        .then(pincollection => {
 
-  map_id = req.params.id
-
-  db.getMapIdByname(req.query.map)
-    .then((currentMap) => {
-
-
-      dc.getPinsByMap(map_id).then((mapPins) => {
-        db.getSingleMap(map_id).then((currentMap) => {
-          const username = req.session.username;
-          console.log("map", currentMap)
-          console.log("pins", mapPins)
-          const templateVars = {
-            pins: mapPins,
-            user: username,
-            mapName: currentMap,
-          };
-
-          res.render("map", templateVars);
-        });
-      });
+      res.render("map", { mapName: currentMap[0].name, user: username, pins: pincollection });
     })
-    .catch((err) => {
-      console.log(err);
-      // res
-      //   .status(500)
-      //   .json({ error: err.message });
+  })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
     });
 });
 
